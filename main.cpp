955d94348b0586a213ed8b19b79f0d5e4ccf4e4c
Danlaysma <danlaysma45.work@gmail.com>
2023-02-11 21:26:18 +0600

    test

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "box.h"
+int main(){
+    box box1 = box(3,3,3,3,3);
+    box box2 = box(1,1,1,1,1);
+    box box3 = box(4,4,4,4,4);
+    box box4 = box(5,5,5,5,5);
+    box boxes[4]={box1,box2,box3,box4};
+    std :: cout << PuttingIt(boxes,4);
+}
