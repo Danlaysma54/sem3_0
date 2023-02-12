523ecd86fd67032badec3433b4d7d3f535e34b13
Danlaysma54 <danlaysma45.work@gmail.com>
2023-02-12 23:08:33 +0600

    fix methods.cpp

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/danlaysma/CLionProjects/gittest/sem3_0
+# Build directory: /home/danlaysma/CLionProjects/gittest/sem3_0/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
